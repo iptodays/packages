612cb99f1cf707c5fab7225c8898654838a17bd5
iptoday <wangdong1221@outlook.com>
2022-06-06 10:44:05 +0800

    0.0.1

--- event_bus/lib/event_bus.dart
+++ event_bus/lib/event_bus.dart
@@ -2,26 +2,26 @@
  * @Author: iptoday wangdong1221@outlook.com
  * @Date: 2022-05-24 13:15:47
  * @LastEditors: iptoday wangdong1221@outlook.com
- * @LastEditTime: 2022-05-24 17:12:24
- * @FilePath: /packages/event_bus/lib/event_bus.dart
+ * @LastEditTime: 2022-06-06 10:43:40
+ * @FilePath: /event_bus/lib/event_bus.dart
  *
  * Copyright (c) 2022 by iptoday wangdong1221@outlook.com, All Rights Reserved.
  */
 
 library event_bus;
 
-typedef EventCallback = void Function(dynamic arg);
+typedef EventCallback = void Function(dynamic args);
 
-class EventBus {
-  factory EventBus() => _getInstance();
-  static EventBus get instance => _getInstance();
-  static EventBus? _instance;
+class IEventBus {
+  factory IEventBus() => _getInstance();
+  static IEventBus get instance => _getInstance();
+  static IEventBus? _instance;
 
-  EventBus._internal() {
+  IEventBus._internal() {
     // init
   }
-  static EventBus _getInstance() {
-    _instance ??= EventBus._internal();
+  static IEventBus _getInstance() {
+    _instance ??= IEventBus._internal();
     return _instance!;
   }
 
@@ -29,28 +29,28 @@ class EventBus {
 
   /// 添加监听
   void addListener({
-    required String eventKey,
+    required String key,
     required EventCallback callback,
   }) {
-    if (_events[eventKey] != null) {
-      _events[eventKey] = [..._events[eventKey]!, callback];
+    if (_events[key] != null) {
+      _events[key] = [..._events[key]!, callback];
     } else {
-      _events[eventKey] = [callback];
+      _events[key] = [callback];
     }
   }
 
   /// 移除监听
-  void removeListener(String eventKey) {
-    if (_events[eventKey]?.isNotEmpty == true) {
-      List<EventCallback> list = _events[eventKey]!;
+  void removeListener(String key) {
+    if (_events[key]?.isNotEmpty == true) {
+      List<EventCallback> list = _events[key]!;
       list.removeLast();
-      _events[eventKey] = [...list];
+      _events[key] = [...list];
     }
   }
 
   /// 发送事件
-  void commit({required String eventKey, dynamic arg}) {
-    List<EventCallback>? callbacks = _events[eventKey];
+  void commit({required String key, dynamic arg}) {
+    List<EventCallback>? callbacks = _events[key];
     if (callbacks != null) {
       for (var element in callbacks) {
         element(arg);
--- event_bus/test/event_bus_test.dart
+++ event_bus/test/event_bus_test.dart
@@ -1,8 +1,14 @@
+/*
+ * @Author: iptoday wangdong1221@outlook.com
+ * @Date: 2022-05-24 13:32:03
+ * @LastEditors: iptoday wangdong1221@outlook.com
+ * @LastEditTime: 2022-06-06 10:41:12
+ * @FilePath: /event_bus/test/event_bus_test.dart
+ * 
+ * Copyright (c) 2022 by iptoday wangdong1221@outlook.com, All Rights Reserved. 
+ */
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:event_bus/event_bus.dart';
-
 void main() {
-  test('adds one to input values', () {
-  });
+  test('adds one to input values', () {});
 }
