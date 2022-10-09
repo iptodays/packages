90347d6131fbf2efb6277470db31e706639eb0d9
iptoday <wangdong1221@outlook.com>
2022-10-09 14:43:40 +0800

    增加demo

--- imobile_ads/lib/imobile_ads.dart
+++ imobile_ads/lib/imobile_ads.dart
@@ -2,7 +2,7 @@
  * @Author: iptoday wangdong1221@outlook.com
  * @Date: 2022-09-22 15:43:48
  * @LastEditors: iptoday wangdong1221@outlook.com
- * @LastEditTime: 2022-09-22 15:44:49
+ * @LastEditTime: 2022-10-09 11:07:01
  * @FilePath: /imobile_ads/lib/imobile_ads.dart
  * 
  * Copyright (c) 2022 by iptoday wangdong1221@outlook.com, All Rights Reserved. 
@@ -10,5 +10,5 @@
 export 'package:google_mobile_ads/google_mobile_ads.dart';
 export 'package:unity_ads_plugin/unity_ads_plugin.dart';
 
-export 'src/imobile_ads.dart';
+export 'src/imobile_ads_impl.dart';
 export 'src/enum.dart';
--- ioader/test/ioader_test.dart
+++ ioader/test/ioader_test.dart
@@ -1,10 +1,17 @@
+/*
+ * @Author: iptoday wangdong1221@outlook.com
+ * @Date: 2022-09-28 22:46:22
+ * @LastEditors: iptoday wangdong1221@outlook.com
+ * @LastEditTime: 2022-10-09 14:41:07
+ * @FilePath: /ioader/test/ioader_test.dart
+ * 
+ * Copyright (c) 2022 by iptoday wangdong1221@outlook.com, All Rights Reserved.
+ */
 import 'package:flutter_test/flutter_test.dart';
 import 'package:ioader/ioader.dart';
 
 void main() {
   test('adds one to input values', () async {
-    bool result = await Ioader.initialize('123231');
-    print(result);
-    print(Ioader().dir);
+    await Ioader.initialize('123231');
   });
 }
